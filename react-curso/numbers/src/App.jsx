5bb1590954c78eb3316ec1c21d4c2d378b3a5109
bynyck <nicollasnascimento.pc@gmail.com>
2026-09-19 13:12:04 -0300

    map no react

--- react-curso/numbers/src/App.jsx
+++ react-curso/numbers/src/App.jsx
@@ -7,13 +7,17 @@ function App( {title, description} ) {
 
   return(
     <div>
-      <h1>{title} - {number}</h1>
+      <h1>{title}  {number}</h1>
 
       <p>{description}</p>
 
       <h2 className={number >= 0 ? "green" : "red"}>{number >= 0 ? "Positivo" : "negativo"}</h2>
       <input type="number" onChange={({target}) => setNumber(target.value)}/>
       
+
+      <ul>
+        {[1,2,3,4,5,6,7,8,9,10].map(num => number > 0 && <li>{number} x {num} = {number * num}</li> )}
+      </ul>
     </div>
   )
 }
